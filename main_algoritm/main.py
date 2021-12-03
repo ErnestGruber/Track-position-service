4d559e0fe8aaf25b4956adc514cee6fa5ab5fa65
Ernest <ernestgruber666@gmail.com>
2021-12-04 02:42:50 +0300

    main script

--- main_algoritm/main.py
+++ main_algoritm/main.py
@@ -1,20 +1,26 @@
-from main_algoritm.gradient import Gradient
+import time
 
+from main_algoritm.gradient import Gradient
+import requests.exceptions
 def alg_methods(id, first_coord):
     send_data=Gradient(first_coord, id)
-    signal=get_signal_GPS(id) #   надо два значени яхороший или не хороший
-    while signal==norm or average==True:
-        coord=get_coord() #получить координаты текущие два значения
-        send_data.main(coord)
-        normalize() #то что ты показывал НО ИЗ БАЗЫ ДАННЫХ
-        coord_truth=get_average(id) #среднее верно  или нет то что хотел даня
-    while signal==not_norm and coord_truth()== False:
-        while wifi_connection(id)==True:
-            send_data = wifiapi()
-            send_data.wifiapi()
-            coord_truth = get_average(id)
-        get_datagsm
-        coord_truth = get_average(id)
+    sqlData=Postgres()
+    signal=sqlData.getpoweroneGS(id) #   надо два значени яхороший или не хороший
+    while signal>25:
+        coordinat_GPS=sqlData.getOneGPS(id) #получить координаты текущие два значения
+        t=send_data.main(coordinat_GPS)
+        time.sleep(t)
+                                # отправить данные T
+        simplify_points(,0.01)#то что ты показывал НО ИЗ БАЗЫ ДАННЫХ
+    ##    coord_truth=get_average(id) #среднее верно  или нет то что хотел даня
+    while signal<=25:
+        if sqlData.getpowerwifi(id) >10:
+            coordinat_WIFI = wifiapi.mac(sqlData.getMac(id))
+            t = send_data.main(coordinat_WIFI)
+            time.sleep(t)
+        coordinat_GSM=Gsm(GsmApi.getcoord(sqlData.getGSM),GsmApi.getcoord(sqlData.getGSM),-------------).coord()
+        t = send_data.main(coordinat_GSM)
+        time.sleep(t)
     alg_methods(id, first_coord)
 
 

70328ffddc930feb08727e12f0b07952a8cd0d84
biggvladek <zuevvlad55@gmail.com>
2021-12-03 15:07:52 +0300

    finallly commit

---
+++ RDP.py
@@ -0,0 +1,67 @@
+import math
+
+def simplify_points (pts, tolerance):
+    anchor  = 0
+    floater = len(pts) - 1
+    stack   = []
+    keep    = set()
+
+    stack.append((anchor, floater))
+    while stack:
+        anchor, floater = stack.pop()
+
+        # инициализация отрезка
+        if pts[floater] != pts[anchor]:
+            anchorX = float(pts[floater][0] - pts[anchor][0])
+            anchorY = float(pts[floater][1] - pts[anchor][1])
+            seg_len = math.sqrt(anchorX ** 2 + anchorY ** 2)
+            # get the unit vector
+            anchorX /= seg_len
+            anchorY /= seg_len
+        else:
+            anchorX = anchorY = seg_len = 0.0
+
+        # внутренний цикл:
+        max_dist = 0.0
+        farthest = anchor + 1
+        for i in range(anchor + 1, floater):
+            dist_to_seg = 0.0
+            # compare to anchor
+            vecX = float(pts[i][0] - pts[anchor][0])
+            vecY = float(pts[i][1] - pts[anchor][1])
+            seg_len = math.sqrt( vecX ** 2 + vecY ** 2 )
+            # dot product:
+            proj = vecX * anchorX + vecY * anchorY
+            if proj < 0.0:
+                dist_to_seg = seg_len
+            else:
+                # compare to floater
+                vecX = float(pts[i][0] - pts[floater][0])
+                vecY = float(pts[i][1] - pts[floater][1])
+                seg_len = math.sqrt( vecX ** 2 + vecY ** 2 )
+                # dot product:
+                proj = vecX * (-anchorX) + vecY * (-anchorY)
+                if proj < 0.0:
+                    dist_to_seg = seg_len
+                else:  # расстояние от до прямой по теореме Пифагора:
+                    dist_to_seg = math.sqrt(abs(seg_len ** 2 - proj ** 2))
+                if max_dist < dist_to_seg:
+                    max_dist = dist_to_seg
+                    farthest = i
+
+        if max_dist <= tolerance: # использование отрезка
+            keep.add(anchor)
+            keep.add(floater)
+        else:
+            stack.append((anchor, farthest))
+            stack.append((farthest, floater))
+
+    keep = list(keep)
+    keep.sort()
+    return [pts[i] for i in keep]
+
+
+pts=[(59.79702067162942,30.27576664520221),	(59.79425242513046,	30.28949955535846),	(59.793214273255224,30.30117252899128),	(59.791830020320795,30.310098920592853),(59.791483948080945,30.319025312194402),(59.800134673376874,30.324518476256895),
+(59.833332542008314,30.281946454772537),	(59.828493252811164,30.292246137389707),	(59.82192451747603,	30.30117252899128)]
+tolerance=0.01
+print(simplify_points(pts,tolerance))

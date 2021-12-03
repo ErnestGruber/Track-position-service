0d8c49373d15bcbd9f7752b86402d417c5f85c8d
Ernest <ernestgruber666@gmail.com>
2021-12-03 17:54:50 +0300

    анные владику

--- main_algoritm/main.py
+++ main_algoritm/main.py
@@ -3,18 +3,19 @@ from main_algoritm.gradient import Gradient
 
 def alg_methods(id, first_coord):
     send_data=Gradient(first_coord, id)
-    signal=get_signal(id)
-    coord_truth(id)
-    while signal==norm or coord_truth()==True:
-        coord=get_coord()
+    signal=get_signal_GPS(id) #   надо два значени яхороший или не хороший
+    while signal==norm or average==True:
+        coord=get_coord() #получить координаты текущие два значения
         send_data.main(coord)
-        normalize()
-        if signal==norm or coord_truth()==True:
-            get_average()
+        normalize() #то что ты показывал НО ИЗ БАЗЫ ДАННЫХ
+        coord_truth=get_average(id) #среднее верно  или нет то что хотел даня
     while signal==not_norm and coord_truth()== False:
         while wifi_connection(id)==True:
             send_data = wifiapi()
             send_data.wifiapi()
+            coord_truth = get_average(id)
         get_datagsm
-    alg_methods(id,first_coord)
+        coord_truth = get_average(id)
+    alg_methods(id, first_coord)
+
 

6dcec8d1f251e9804b3bf5f188000ce5becca756
biggvladek <zuevvlad55@gmail.com>
2021-12-04 07:29:08 +0300

    Merge remote-tracking branch 'origin/test' into test
    # Conflicts:
    #	GSM/gsm.py
    #	main_algoritm/normilized.py

--- main_algoritm/main.py
+++ main_algoritm/main.py
@@ -22,7 +22,20 @@ def alg_methods(id, first_coord):
             coordinat_WIFI = WiFi.wifiapi.getcoord(sqlData.getMac(id))
             t = send_data.main(coordinat_WIFI)
             time.sleep(t)
-        coordinat_GSM = Gsm(GsmApi.getcoord(sqlData.getGSM), GsmApi.getcoord(sqlData.getGSM), sqlData.gsmpower().coord(),1,1,)
+            w = Postgres.getGSM(1)
+            a = GsmApi.getcoord(w[0], w[1], w[2], w[3])
+            x1, y1 = a[0], a[1]
+            z = Postgres.getGSM(2)
+            b = GsmApi.getcoord(z[0], z[1], z[2], z[3])
+            x2, y2 = b[0], b[1]
+            p = Postgres.getGSM(2)
+            c = GsmApi.getcoord(p[0], p[1], p[2], p[3])
+            x3, y3 = c[0], c[1]
+            x = [x1, x2, x3]
+            y = [y1, y2, y3]
+            out = Postgres.gsmpower(1)
+            a = Gsm(x, y, out)
+            coordinat_GSM = a.coord()
         t = send_data.main(coordinat_GSM)
         time.sleep(t)
     alg_methods(id, first_coord)
